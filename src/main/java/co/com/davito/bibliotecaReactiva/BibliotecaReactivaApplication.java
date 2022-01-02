5fe8504ac7e60cc506049400bb131223bdf7d251
Davito19 <omardbonett@gmail.com>
2022-01-02 13:10:13 -0500

    git init

---
+++ src/main/java/co/com/davito/bibliotecaReactiva/BibliotecaReactivaApplication.java
@@ -0,0 +1,13 @@
+package co.com.davito.bibliotecaReactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaReactivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/davito/bibliotecaReactiva/BibliotecaReactivaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.davito.bibliotecaReactiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaReactivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
